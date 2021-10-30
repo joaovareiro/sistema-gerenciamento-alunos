ac5b0d5bf2e9e7c63eac9b4f5e4407c943a8c666
joaovareiro <joaovareiro1@gmail.com>
2021-10-30 13:57:11 -0400

    quick fix

---
+++ aluno.cpp
@@ -0,0 +1,42 @@
+#include stdio.h
+#include aluno.h
+
+void addAluno(tAlunoEntrada vetAlunos, int &n, char mod, int RA, float p1, float p2, float pt, float po)
+{
+	strcpy(vetAlunos[n].nome, mod);
+	vetAlunos[n].RA = RA;
+	vetAlunos[n].p1 = p1;
+	vetAlunos[n].p2 = p2;
+	vetAlunos[n].pt = pt;
+	vetAlunos[n].po = po;
+	n++;
+
+}
+void GeraAprovados(){
+FILE pont_aprov;
+pont_aprov = fopen(aprovados.txt, a);
+fclose(pont_aprov);
+printf(O arquivo com os alunos aprovados foi criado com sucesso!);
+}
+
+void GeraReprovados(){
+FILE pont_reprov;
+pont_reprov = fopen(Reprovados.txt, a);
+fclose(pont_reprov);
+printf(O arquivo com os alunos reprovados foi criado com sucesso!);
+}
+
+void abreArquivo(){
+
+printf(Digite o nome do arquivo que contem os alunos);
+FILE arqEntrada;
+char nomeArq[MAX];
+scanf( %s, nomeArq);
+
+arqEntrada = fopen(nomeArq, r);
+
+if( arqEntrada == NULL ){
+  printf(nn Arquivo %s nao pode ser aberto.nn, nomeArq);
+}else{
+	adicionar alguma forma de os dados serem adicionados no vetor
+}
